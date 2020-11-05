897cc8de99397aa8efa02fc90d7c3e1c9eaa5ef4
Vexation <73531576+Vexation@users.noreply.github.com>
2020-11-05 16:55:39 +0200

    Add files via upload

---
+++ Interfaces/Models/Boat.cs
@@ -0,0 +1,44 @@
+﻿using Interfaces.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Models
+{
+    class Boat : AutoMobile, IAutoMobileActions, IBoatActions
+    {
+
+        public Boat()
+        {
+
+        }
+
+        public Boat(bool IsOn)
+        {
+            this.IsOn = IsOn;
+        }
+
+        public void SteerLeft()
+        {
+            Console.WriteLine(" steering left");
+
+        }
+
+        public void Reverse()
+        {
+            Console.WriteLine("The boat is reversing");
+        }
+
+        public void SteerRight()
+        {
+            Console.WriteLine("steering right");
+        }
+
+        public void Steer(bool steerLeft)
+        {
+            Console.WriteLine("steering left");
+        }
+    }
+}
---
+++ Interfaces/Models/Car.cs
@@ -0,0 +1,47 @@
+﻿using Interfaces.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Models
+{
+    class Car : AutoMobile, IAutoMobileActions, ICarActions
+    {
+		public Car()
+		{
+
+		}
+
+		public Car(bool IsOn)
+		{
+			this.IsOn = IsOn;
+		}
+
+		public void SteerLeft()
+		{
+			Console.WriteLine("The car is steering left");
+		}
+
+		public void Break()
+		{
+			Console.WriteLine("The car is breaking");
+		}
+
+		public void Reverse()
+		{
+			Console.WriteLine("The car is reversing");
+		}
+
+		public void SteerRight()
+		{
+			Console.WriteLine("The car is steering right");
+		}
+
+		public void Steer(bool steerLeft)
+		{
+			Console.WriteLine("The car is steering left");
+		}
+	}
+}
